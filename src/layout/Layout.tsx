f0335331107c553d4e32f494e75dfd8f55c479b7
Jeremy Viallatt <shoo>
2022-11-04 18:14:22 +0100

    Small change to layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,22 +4,12 @@ import TopBar from '../components/cellules/TopBar/TopBar'
 
 const Layout: FC = () => {
   return (
-      <div className="layout">
-          <TopBar />
-          <main className='layout_wrapper'>
-              <Outlet />
-          </main>
-          <nav>
-          <ul>
-            <li>
-              <a href={'/'}>All Media</a>
-            </li>
-            <li>
-              <a href={'media'}>Media Detail</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className="layout">
+      <TopBar />
+      <main className='layout_wrapper'>
+        <Outlet />
+      </main>
+    </div>
   )
 }
 
